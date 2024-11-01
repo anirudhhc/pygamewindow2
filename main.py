3fa887c1c1d00600e206f2679740cbc97bf47336
anirudhhc <hcanirudh@gmail.com>
2024-11-01 17:36:53 +0530

    file54

---
+++ main.py
@@ -0,0 +1,10 @@
+# Import necessary libraries
+from tkinter import *
+
+#create a window
+window = Tk()
+
+#set the window Title and geomentry
+window.title('Demo window')
+window.geometry('400x300')
+window.mainloop()
